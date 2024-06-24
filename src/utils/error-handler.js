7567571cc07fbbd16d4e9e8db057a7ae78afbd13
abelalemujotie <abelalemu155@gmail.com>
2024-06-24 17:44:37 +0300

    Revert "fix: concise code"
    This reverts commit cfa326db10def98367dc3d3241c75bbea937e60f.

--- src/utils/error-handler.js
+++ src/utils/error-handler.js
@@ -1,8 +1,8 @@
 
 const ERR_CONSTANTS= require('../constants').ERR_CONSTANTS; 
 
-const errModule= (err,_,res,_)=> {
-
+const errModule= (err,req,res,next)=> {
+    // cheking the error message passed the error middle ware
     switch(err.message){
         case(ERR_CONSTANTS.badRequest):
             const body= err.body ?? '';
@@ -15,6 +15,7 @@ const errModule= (err,_,res,_)=> {
             res.status(403).json({'error': 'authorization failed'}); 
             break;
         case(ERR_CONSTANTS.notFound):
+           console.log("cehcking");
            res.status(404).json({'error': 'resource not found'}); 
            break; 
         default: 
