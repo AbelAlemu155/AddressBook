27a43f986207d3541d424c1ab01ba806bfa254ac
AbelAlemu155 <56737740+AbelAlemu155@users.noreply.github.com>
2024-09-01 16:42:17 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const addRouter= require("./routes/address-entries");
 
 const app= express();
 const errModule= require('./utils/error-handler');  
-const port=3000;
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
